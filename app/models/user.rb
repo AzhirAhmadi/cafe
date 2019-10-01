4486592a0f2710465f6eba72920b85eb685f52e4
AzhirAhmadi <aaahmadi99@gmail.com>
2019-10-01 16:46:13 +0330

    -add role to user table as enum -enum elements are [:sys_master, :sys_admin, :sys_expert, :cafe_ower,:player]

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :validatable
 
+  #JWT config
   before_create :add_jti
   def add_jti
   self.jti ||= SecureRandom.uuid
+  end
+
+
+  #role config
+  enum role: [:sys_master, :sys_admin, :sys_expert, :cafe_ower,:player]
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    self.role ||= :player
   end
 end
