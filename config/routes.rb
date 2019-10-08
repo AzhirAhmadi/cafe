bf3e0c67b44d0d0ea83f5b6fa86749ccb5c83316
AzhirAhmadi <aaahmadi99@gmail.com>
2019-10-08 18:33:05 +0330

    change devise routing

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,9 @@
 #
 
 Rails.application.routes.draw do
+  devise_for :users, class_name: 'ApiUser', skip: :all
   scope path: 'api' do
-    devise_for :users, class_name: 'ApiUser', skip: :all
+    
     devise_scope :user do
       post    'login',        to: 'devise_api/sessions#create'
       delete  'logout',       to: 'devise_api/sessions#destroy'
