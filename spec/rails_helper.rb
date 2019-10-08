3ca0e129d0550ee621e8b430cf056b46eba95fe8
AzhirAhmadi <aaahmadi99@gmail.com>
2019-10-08 18:34:23 +0330

    config rails_helper for useing devise in tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'devise'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -20,7 +21,7 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -31,9 +32,6 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
-  #my configs
-  config.include FactoryBot::Syntax::Methods
-
 
 
 
@@ -64,4 +62,18 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  #my configs
+  config.include FactoryBot::Syntax::Methods
+  config.include JsonApiHelpers
+  config.include SharedHelper
+  config.include DeviseHelper
+  # config.expect_with :rspec do |c|
+  #   c.syntax = :expect
+  # end
+
+  config.include Devise::Test::ControllerHelpers, :type => :controller
+
+  # config.include Warden::Test::Helpers
+
 end
