735bd372648d5184c2ce25f478010e0a7ba14ddb
AzhirAhmadi <aaahmadi99@gmail.com>
2019-10-14 11:43:00 +0330

    change login & logout for 'request' spec type

--- spec/support/devise_helper.rb
+++ spec/support/devise_helper.rb
@@ -1,8 +1,7 @@
 module DeviseHelper
     @token
     def login user
-        @request.env["devise.mapping"] = Devise.mappings[:user]
-        post :create, params: {"user": {"email": user.email, "password": user.password }}, format: :json
+        post URL(login_path), params: {"user": {"email": user.email, "password": user.password }}
         @token = JSON.parse(response.body)["jwt"]
         response
     end
@@ -10,12 +9,24 @@ module DeviseHelper
     def logout
         @request.env["devise.mapping"] = Devise.mappings[:user]
         @request.headers["Authorization"] = token
-        delete :destroy, params: {}, format: :json
+        delete URL(logout_path)
         @token = nil
     end
 
     def token
         @token
+    end
+
+    def authorized_request method, params: nil
+        @request.env["devise.mapping"] = Devise.mappings[:user]
+        @request.headers["Authorization"] = token
+
+        case method
+        when "post"
+            post :create, params: params, format: :json
+        when "delete"
+            delete :destroy, params: params, format: :json
+        end
     end
 
 end
