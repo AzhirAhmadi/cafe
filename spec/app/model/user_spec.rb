b8844a4ff04e58b497d9a1c8ca56edb167b5a330
AzhirAhmadi <aaahmadi99@gmail.com>
2019-10-15 22:45:21 +0330

    change user's factory symbole from ':user' to ':player'

--- spec/app/model/user_spec.rb
+++ spec/app/model/user_spec.rb
@@ -5,34 +5,34 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe "#validations" do
     it "shuold have valid factory" do
-      expect(build :user).to be_valid
+      expect(build :player).to be_valid
     end
 
     it "should validate presence of attributes" do
-      user = build :user, email: nil, password: nil
+      user = build :player, email: nil, password: nil
       expect(user).not_to be_valid
       expect(user.errors.messages[:email]).to include("can't be blank")
       expect(user.errors.messages[:password]).to include("can't be blank")
     end
 
     it "should validate length of :password (minimum 6)" do
-      user = build :user, email: "test@test.com", password: "1"
+      user = build :player, email: "test@test.com", password: "1"
       expect(user).not_to be_valid
       expect(user.errors.messages[:password]).to include("is too short (minimum is 6 characters)")
     end
 
     it "should validate :email format (*@*)" do
-      user = build :user, email: "invalid", password: "123456"
+      user = build :player, email: "invalid", password: "123456"
       expect(user).not_to be_valid
       expect(user.errors.messages[:email]).to include("is invalid")
     end
 
     it "should validate uniqness of :email" do
-      user = build :user, email: "test@test.com", password: "123456"
+      user = build :player, email: "test@test.com", password: "123456"
       user.save
       expect(user.save).to be(true)
 
-      otherUser = build :user, email: "test@test.com", password: "123456"
+      otherUser = build :player, email: "test@test.com", password: "123456"
       expect(otherUser).not_to be_valid
       expect(otherUser.errors.messages[:email]).to include("has already been taken")
     end
