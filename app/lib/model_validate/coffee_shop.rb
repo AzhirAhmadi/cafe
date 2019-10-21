0a74650d8fcdef8ea47e86297c187ceebcf070a6
AzhirAhmadi <aaahmadi99@gmail.com>
2019-10-21 14:41:43 +0330

    add maintainer validations

--- app/lib/model_validate/coffee_shop.rb
+++ app/lib/model_validate/coffee_shop.rb
@@ -2,16 +2,24 @@ module ModelValidate::CoffeeShop
     def active_owner
         errors.add(:owner, "is not active") unless  owner&.active?
     end
-    
+
+    def active_maintainer
+        errors.add(:maintainer, "is not active") unless  maintainer&.active?
+    end
+
     def active_creator
         errors.add(:creator, "is not active") unless  creator&.active?
     end
 
     def owner_role
-        errors.add(:owner, "role must be 'cafe_owner'") unless  owner&.cafe_owner?
+        errors.add(:owner, "role must be 'coffee_owner'") unless  owner&.coffee_owner?
+    end 
+
+    def maintainer_role
+        errors.add(:maintainer, "role must be 'sys_expert'") unless  maintainer&.sys_expert?
     end 
 
     def creator_role
-        errors.add(:creator, "role must be 'sys_admin'") unless  creator&.sys_admin?
+        errors.add(:creator, "role must be 'sys_admin'") unless  creator&.sys_admin? || creator&.sys_master?
     end 
 end
