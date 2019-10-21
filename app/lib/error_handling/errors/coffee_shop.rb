e184681ca9e23f0c9b9d4d9336e8876f6169b0b2
AzhirAhmadi <aaahmadi99@gmail.com>
2019-10-21 14:37:32 +0330

    correct coffee_shop error handler

--- app/lib/error_handling/errors/coffee_shop.rb
+++ app/lib/error_handling/errors/coffee_shop.rb
@@ -1,25 +1,31 @@
 module ErrorHandling::Errors::CoffeeShop
     class DataBaseCreation < ErrorHandling::Error
         def message
-            args[:user].errors
+            args[:coffee_shop].errors
+        end
+    end
+
+    class DataBaseUpdate < ErrorHandling::Error
+        def message
+            args[:coffee_shop].errors
         end
     end
 
     class CreationParams < ErrorHandling::Error
         def message
-            "params for create coffee_shop must be provided like this: {\"coffee_shop\" :{\"name\" : \"coffee_shop_name\", \"address\":\"coffee_shop_address\", \"owner_id\": \"coffee_shop_owner_id\"}}"
+            "params for create coffee_shop must be provided like this: {\"coffee_shop\" :{\"name\" : \"coffee_shop_name\", \"address\":\"coffee_shop_address\", \"owner_id\": \"coffee_shop_owner_id\", \"maintainer_id\": \"coffee_shop_maintainer_id\"}}"
         end
     end
 
     class UpdateParams < ErrorHandling::Error
         def message
-            "params for update coffee_shop must be provided like this: {\"coffee_shop\" :{\"name\" : \"coffee_shop_name\", \"address\":\"coffee_shop_address\", \"owner_id\": \"coffee_shop_owner_id\"}}"
+            "params for update coffee_shop must be provided like this: {\"coffee_shop\" :{\"name\" : \"coffee_shop_name\", \"address\":\"coffee_shop_address\", \"owner_id\": \"coffee_shop_owner_id\", \"maintainer_id\": \"coffee_shop_maintainer_id\"}}"
         end
     end
 
-    class DeletedUser < ErrorHandling::Error
+    class DeletedCoffeeShop < ErrorHandling::Error
         def message
-           "Your account has been deleted at: [" + args[:deleted_at].to_s + "]"
+           "This coffee shop has been deleted at: [" + args[:deleted_at].to_s + "]"
         end
     end
 end
