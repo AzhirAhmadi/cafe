e47cbd3ff5a2b8d056125e76c346eb01ded110fc
AzhirAhmadi <aaahmadi99@gmail.com>
2019-10-26 15:55:53 +0330

    add test for sys_expert role in events_controller test folder

--- app/lib/param_sanitizer/events_params.rb
+++ app/lib/param_sanitizer/events_params.rb
@@ -3,13 +3,13 @@ class  ParamSanitizer::EventsParams < ParamSanitizer::ParamSanitizer
     #           5               4               3               2               1
     # role: [   :sys_master,    :sys_admin,     :sys_expert,    :coffee_owner,    :player]
     def create_params
-        if current_user.coffee_owner?
+        if current_user.coffee_owner? || current_user.sys_expert?
             return [:name, :description, :opens_at, :enrol_start_time, :enrol_end_time, :event_start_time, :event_end_time, :closed_at] 
         end
     end
 
     def update_params
-        if current_user.coffee_owner?
+        if current_user.coffee_owner? || current_user.sys_expert?
             return [:name, :description, :opens_at, :enrol_start_time, :enrol_end_time, :event_start_time, :event_end_time, :closed_at] 
         end
     end
--- app/policies/event_policy.rb
+++ app/policies/event_policy.rb
@@ -2,14 +2,17 @@ class EventPolicy < ApplicationPolicy
     # enum role: [:sys_master, :sys_admin, :sys_expert, :coffee_owner,:player]
   def create?
     return true if  current_user.coffee_owner? && event.coffee_shop.owner.id == current_user.id
+    return true if  current_user.sys_expert? && event.coffee_shop.maintainer.id == current_user.id
   end
 
   def update?
     return true if  current_user.coffee_owner? && event.coffee_shop.owner.id == current_user.id
+    return true if  current_user.sys_expert? && event.coffee_shop.maintainer.id == current_user.id
   end
 
   def deactivate?
     return true if  current_user.coffee_owner? && event.coffee_shop.owner.id == current_user.id
+    return true if  current_user.sys_expert? && event.coffee_shop.maintainer.id == current_user.id
   end
 
 
