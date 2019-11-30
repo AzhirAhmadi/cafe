34a7d0a7601803b146572ec92285fea64ab12232
AzhirAhmadi <aaahmadi99@gmail.com>
2019-11-30 13:45:22 +0330

    add codes for debuging

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,17 +15,25 @@
 # FactoryBot.create :opened_event, coffee_shop: CoffeeShop.find(2)
 # FactoryBot.create :opened_event, coffee_shop: CoffeeShop.find(2)
 
-coffee_shop =  FactoryBot.create :coffee_shop
-opened_event = FactoryBot.create :opened_event, coffee_shop: coffee_shop
+# coffee_shop =  FactoryBot.create :coffee_shop
+# opened_event = FactoryBot.create :opened_event, coffee_shop: coffee_shop
 
-boardgame1 = FactoryBot.create :board_game, coffee_shop: coffee_shop
-boardgame2 = FactoryBot.create :board_game, coffee_shop: coffee_shop
-boardgame3 = FactoryBot.create :board_game, coffee_shop: coffee_shop
-boardgame4 = FactoryBot.create :board_game, coffee_shop: coffee_shop
-boardgame5 = FactoryBot.create :board_game, coffee_shop: coffee_shop
+# boardgame1 = FactoryBot.create :board_game, coffee_shop: coffee_shop
+# boardgame2 = FactoryBot.create :board_game, coffee_shop: coffee_shop
+# boardgame3 = FactoryBot.create :board_game, coffee_shop: coffee_shop
+# boardgame4 = FactoryBot.create :board_game, coffee_shop: coffee_shop
+# boardgame5 = FactoryBot.create :board_game, coffee_shop: coffee_shop
 
-FactoryBot.create :table, event: opened_event, board_game: boardgame1
-FactoryBot.create :table, event: opened_event, board_game: boardgame2
-FactoryBot.create :table, event: opened_event, board_game: boardgame3
-FactoryBot.create :table, event: opened_event, board_game: boardgame4
-FactoryBot.create :table, event: opened_event, board_game: boardgame5
+# FactoryBot.create :table, event: opened_event, board_game: boardgame1
+# FactoryBot.create :table, event: opened_event, board_game: boardgame2
+# FactoryBot.create :table, event: opened_event, board_game: boardgame3
+# FactoryBot.create :table, event: opened_event, board_game: boardgame4
+# FactoryBot.create :table, event: opened_event, board_game: boardgame5
+# coffee_shop = CoffeeShop.find(7)
+
+# FactoryBot.create :locked_event, coffee_shop: coffee_shop
+# FactoryBot.create :opened_event, coffee_shop: coffee_shop
+# FactoryBot.create :enroled_event, coffee_shop: coffee_shop
+# FactoryBot.create :started_event, coffee_shop: coffee_shop
+# FactoryBot.create :ended_event, coffee_shop: coffee_shop
+# FactoryBot.create :closed_event, coffee_shop: coffee_shop
